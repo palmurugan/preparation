95d18437b8ca97be0be0ce0c5ae6750fbd6bf62b
Palmurugan  Chandran <palmuruganc@suntecsbs.com>
2021-02-15 12:40:07 +0530

    initial commit

---
+++ invoiceservice/src/main/java/com/pal/invoiceservice/InvoiceserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pal.invoiceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceserviceApplication.class, args);
+	}
+
+}
